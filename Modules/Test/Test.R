7d9997a27282b1f2d953e051573b6cfed03c97af
Chi Po Choi <chipo.choi@bayer.com>
2023-06-19 14:47:46 +0000

    delete unused stuff in Test.R

--- Modules/Test/Test.R
+++ Modules/Test/Test.R
@@ -17,8 +17,6 @@ source("../Metrics/marginal_rank_change.R")
 # Functions for generating random polynomials.
 #
 
-source("./simulate.R")
-
 generate_poly_coef = function(u_PEDIGREE_NAME, poly_max_degree, poly_sigma) {
   matrix(rnorm(length(u_PEDIGREE_NAME)*(poly_max_degree+1), sd = poly_sigma),
          nrow = length(u_PEDIGREE_NAME),
@@ -173,92 +171,3 @@ fitted_model$df %>%
   pivot_wider(names_from = current_harvest_repetition, values_from = marginal_rank_change) %>% print(width=Inf)
 
 fitted_to_genetic_gain(fitted_model$df, seq(0.1, 0.5, by=0.1))
-
-parallel_fit_lme_poly = function(df, poly_max_degree, range_max_r) {
-  max_repetition = df %>% summarise(max_repetition = max(as.numeric(REPETITION))) %>% pull(max_repetition)
-  poly_matrix = generate_poly(max_repetition, poly_max_degree)
-  
-  formula_string = paste("TRAIT_VALUE ~ 0 + GROWSEASON + FIELD_NAME + (0 +", paste(paste0("poly", 0:poly_max_degree), collapse = " + "), "| PEDIGREE_NAME) + ( poly0 | PLOT_BID:PEDIGREE_NAME)", collapse = "")
-  formula_string_re = paste("~(0 +", paste(paste0("poly", 0:poly_max_degree), collapse = " + "), "| PEDIGREE_NAME)", collapse = "")
-  cat("range_max_r = ", range_max_r, "\n")
-  print("hello")
-  output = foreach(max_r=range_max_r, .packages = c("tidyverse", "stringr", "lme4")) %dopar% {
-    cat("Fitting up to repetitions", max_r, "\n")
-    df_max_r = df %>%
-      mutate(REPETITION = as.integer(REPETITION)) %>%
-      filter(REPETITION <= max_r) %>%
-      left_join(poly_matrix %>%
-                  data.frame() %>%
-                  rownames_to_column("REPETITION") %>%
-                  mutate(REPETITION = as.integer(REPETITION)),
-                by = "REPETITION")
-    m_max_r = lmer(formula(formula_string),
-                   data = df_max_r)
-    m_max_r@frame = m_max_r@frame %>% mutate(REPETITION = df_max_r$REPETITION)
-    df_max_r = df_max_r %>%
-      mutate(current_max_r = max_r,
-             skip = "SKIP_NONE",
-             fitted_value = predict(m_max_r),
-             fitted_value_fe = predict(m_max_r, re.form = ~ 0),
-             fitted_value_re = predict(m_max_r, re.form = formula(formula_string_re), random.only = T))
-    list(df = df_max_r, model = m_max_r)
-  }
-  return(output)
-}
-
-parallel_fit_lme_poly2 = function(df, poly_max_degree, range_max_r) {
-  max_repetition = df %>% summarise(max_repetition = max(as.numeric(REPETITION))) %>% pull(max_repetition)
-  poly_matrix = generate_poly(max_repetition, poly_max_degree)
-  
-  formula_string = paste("TRAIT_VALUE ~ 0 + GROWSEASON + FIELD_NAME + (0 +", paste(paste0("poly", 0:poly_max_degree), collapse = " + "), "| PEDIGREE_NAME) + ( poly0 | PLOT_BID:PEDIGREE_NAME)", collapse = "")
-  formula_string_re = paste("~(0 +", paste(paste0("poly", 0:poly_max_degree), collapse = " + "), "| PEDIGREE_NAME)", collapse = "")
-  cat("range_max_r = ", range_max_r, "\n")
-  start_time <- Sys.time()
-  output = mclapply(range_max_r, function(max_r) {
-    cat("Fitting up to repetitions", max_r, "\n")
-    df_max_r = df %>%
-      mutate(REPETITION = as.integer(REPETITION)) %>%
-      filter(REPETITION <= max_r) %>%
-      left_join(poly_matrix %>%
-                  data.frame() %>%
-                  rownames_to_column("REPETITION") %>%
-                  mutate(REPETITION = as.integer(REPETITION)),
-                by = "REPETITION")
-    m_max_r = lmer(formula(formula_string),
-                   data = df_max_r)
-    m_max_r@frame = m_max_r@frame %>% mutate(REPETITION = df_max_r$REPETITION)
-    df_max_r = df_max_r %>%
-      mutate(current_max_r = max_r,
-             skip = "SKIP_NONE",
-             fitted_value = predict(m_max_r),
-             fitted_value_fe = predict(m_max_r, re.form = ~ 0),
-             fitted_value_re = predict(m_max_r, re.form = formula(formula_string_re), random.only = T))
-    list(df = df_max_r, model = m_max_r)
-  }, mc.cores = 8)
-  end_time <- Sys.time()
-  cat("time elapsed = ", end_time - start_time, "mins.\n")
-  return(output)
-}
-
-
-fit_lme_single = function(df, max_r, poly_matrix, formula_string, formula_string_re) {
-  cat("Fitting up to repetitions", max_r, "\n")
-  df = df %>%
-    mutate(REPETITION = as.integer(REPETITION)) %>%
-    filter(REPETITION <= max_r) %>%
-    left_join(poly_matrix %>%
-                data.frame() %>%
-                rownames_to_column("REPETITION") %>%
-                mutate(REPETITION = as.integer(REPETITION)),
-              by = "REPETITION")
-  m = lmer(formula(formula_string),
-           data = df)
-  m@frame = m@frame %>% mutate(REPETITION = df$REPETITION)
-  df = df %>%
-    mutate(current_max_r = max_r,
-           skip = "SKIP_NONE",
-           fitted_value = predict(m),
-           fitted_value_fe = predict(m, re.form = ~ 0),
-           fitted_value_re = predict(m, re.form = formula(formula_string_re), random.only = T))
-  return(list(df = df, model = m))
-}
